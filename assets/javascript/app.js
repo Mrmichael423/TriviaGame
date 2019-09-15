748187282ad45a1f921ed2fcf3721f6e13aedf29
Kevin Halstead <mrmichael423@icloud.com>
2019-09-14 19:40:34 -0600

    added js and ccs and linked them to the html

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+//var for right and wrong answers
+//var for time limit
+//var with a boolean to see if the answer was right
+//var with an object containing an array with questions and answers
+// function containing a for loop for asking questions
+// if/else statement to show right and wrong answers
+// reset function
